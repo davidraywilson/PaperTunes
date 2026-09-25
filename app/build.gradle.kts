5f7526d00330138f74a2b17ad69c128f4c090d0c
David Wilson <davidray.real@gmail.com>
2026-09-25 08:41:48 -0500

    eink: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
     applicationId = "moe.rukamori.archivetune"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
